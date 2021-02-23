137a6192c672f4b44c7902b2132a168c6ed1d609
RianneStreef <riannestreef@gmail.com>
2021-02-23 17:53:06 +0100

    update

---
+++ sw.js
@@ -0,0 +1,36 @@
+console.log('service worker running');
+
+var cacheName = '/xxio-v1';
+var contentToCache = [
+'../index.html',
+'../categories.html',
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log('[Service Worker] Fetched resource '+e.request.url);
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
